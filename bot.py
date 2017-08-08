834cd87f7b945d666a4325754b3894be568acd72
SL0RD <sidney@demolitor.ca>
2017-08-07 21:37:32 -0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+import sys
+import os
+import pickle
+import time
+import random
+
+TOKEN = "103657961:AAH4j7e--rba3f66mLpaDywtNltpuS5R9Aw"
+MODULE_DIR = "{0}/modules/".format(os.getcwd())
+dp = ""
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text="IM BACK BITCHES, no commands yet.")
+
+
+def main():
+    global dp
+
+    updater = Updater(token=TOKEN)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("help", help))
+
+    updater.start_polling(timeout=5)
+
+    while True:
+        text = raw_input()
+
+        if text == "stop":
+            updater.stop()
+            break
+
+
+if __name__ == '__main__':
+    main()
