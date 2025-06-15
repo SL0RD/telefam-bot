6f689c5874bf9ce35886c092e4745427340d669d
Sidney deMolitor <sidney@demolitor.ca>
2025-06-15 15:40:34 -0300

    Moved weather and help to module and added basic logic for dynamically loading all modules in module folder

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@
 
 import logging
 import sys
+import importlib
 import os
 import time
 import requests
 
 import config
-from decimal import Decimal
+
 from telegram import Update
 from telegram.ext import (
         Application,
@@ -17,21 +18,13 @@ from telegram.ext import (
         filters,
         )
 
-from forex_python.converter import CurrencyRates
-from pyowm import OWM
-# from pyowm.utils import config as owmconfig
-# from pyowm.utils import timestamps
-
+global config, modules
+config = config
 TOKEN = config.TOKEN
 MODULE_DIR = "{0}/modules/".format(os.getcwd())
 
-COUNTRY_CODES = ['CA','US']
-STATE_CODES = ['AL','AK','AR','AZ','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IA','IN','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
-
-owm = OWM(config.OWM)
-mgr = owm.weather_manager()
-reg = owm.city_id_registry()
-#zip = mgr.weather_at_zip_code()
+modules = {}
+commands = []
 ERapiurl = f"https://v6.exchangerate-api.com/v6/{config.er_api_key}/latest/USD"
 
 root = logging.getLogger()
@@ -52,44 +45,78 @@ logger.setLevel(logging.DEBUG)
 
 os.makedirs("chatlogs", exist_ok = True)
 
+
+def getLFName(messagedata):
+    strtime = time.strftime("%Y%m%d")
+    filename = messagedata.title + "-" + strtime + ".log"
+    return filename
+
+
 def writechatLog(logmsg, file):
     fullpath = "chatlogs/" + file
     with open(fullpath, 'a') as logfile:
         logfile.write(logmsg + '\n')
 
+module_files = [f[:-3] for f in os.listdir(MODULE_DIR) if f.endswith(".py") and f.startswith("module_")]
 
-def getLFName(groupname):
-    strtime = time.strftime("%Y%m%d")
-    filename = groupname.title + "-" + strtime + ".log"
-    return filename
+for module_name in module_files:
+    spec = importlib.util.spec_from_file_location(module_name, os.path.join(MODULE_DIR, f"{module_name}.py"))
+    module = importlib.util.module_from_spec(spec)
+    #importlib.import_module(module_name)
+    spec.loader.create_module(module)
+    spec.loader.exec_module(module)
+    modules[module_name] = module
 
 
-def getLocation(location):
-    location = " ".join(location.split()[1:])
-    if len(location.split()[-1]) == 2:
-        country = location.split()[-1].upper()
-        location = " ".join(location.split()[:-1])
-        print(country)
-        print("Found country code")
-        if len(location.split()[-1]) == 2:
-            print ("Found state code")
-            state = location.split()[-1]
-            print(state)
-            location = " ".join(location.split()[:-1])
-            print(location)
-            locations = reg.locations_for(location, country=country, state=state, matching='exact')
-        else:
-            print(location)
-            locations = reg.locations_for(location, country=country, matching='exact')
-    else:
-        print("Error: no Country code found.")
-        country = ""
+for module in modules:
+    print(module)
 
-    
-    if locations:
-        return locations[0]
-    else:
-        return "Error, location not found"
+def loadcommands(rehash=False):
+    """Load commands from modules"""
+    global application, modules, commands
+    if rehash:
+        handlers = application.handlers
+        for handler in handlers[0]:
+            if isinstance(handler, CommandHandler):
+                if handler.callback.__name__[:-8] in commands:
+                    logger.info("Unloading command: {}".format(handler.callback.__name__[:-8]))
+                    application.remove_handler(handler)
+                    commands.remove(handler.callback.__name__[:-8])
+        for module_name, module in modules.items():
+            logger.info("Reloading module: {}".format(module_name))
+    #        importlib.reload(module)
+        
+        sleep(5)
+        
+        """
+        for m, env in modules.items():
+            myglobals, mylocals, = env
+            commands = [(c, ref) for c, ref in mylocals.items()
+                        if c.endswith("_command")]
+            print(commands)
+            for (t, f) in commands:
+                print(f)
+                logger.info("Unloading command: {}".format(t[:-8]))
+                application.remove_handler(CommandHandler(t[:-8], f))
+"""
+    for module_name, module in modules.items():
+        logger.info("Loading commands from: {}".format(module_name))
+        for c in (dir(module)):
+            if c.endswith("_command"):
+                command = getattr(module, c)
+                logger.info("Loading command: {}".format(c[:-8]))
+                application.add_handler(CommandHandler(c[:-8], command))
+                commands.append(c[:-8])
+
+def loadmodules():
+    """Attempt to load modules from MODULE_DIR"""
+    global modules
+    modules = {}
+    for module in findmodules():
+        env = {}
+        logging.info("Loading module - {}".format(module))
+        exec(open(os.path.join(MODULE_DIR, module)).read(), env, env)
+        modules[module] = (env, env)
 
 
 async def usd_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -120,8 +147,8 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued"""
     await update.effective_message.reply_html(
-            f"Your chat ID is: <code>{update.effective_chat.id}</code>"
-            )
+        f"Your chat ID is: <code>{update.effective_chat.id}</code>"
+    )
 
 
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -129,46 +156,11 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
     logger.error("Exception while handling an update:", exc_info=context.error)
 
 
-async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Get current weather for given location."""
-    message = update.message.text
-    if len(message.split()) > 1:
-        if message.split()[1].isdigit():
-            zipcode = message.split()[1]
-            weather = mgr.weather_at_zip_code(zipcode,'US').weather
-            print(weather)
-            temperature = weather.temperature('fahrenheit')
-            wind = str(round(weather.wind(unit='miles_hour')['speed'],2)) + "mph"
-            fltemp = str(temperature['feels_like']) + "\u00b0 F"
-            rtemp = str(temperature['temp']) + "\u00b0 F"
-
-            forcast = "It is currently {0} and feels like {1}\n"\
-                      "The actual temperature is: {2} with a windspeed of {3}\n"\
-                      "The sun will set at {4}".format(weather.detailed_status,fltemp, rtemp, wind, weather.sunset_time(timeformat='date'))
-            await context.bot.send_message(update.message.chat_id, text=f"{forcast}")
-        else:
-            location = getLocation(update.message.text)
-            if location != "Error, location not found":
-                print(location)
-                weather = mgr.weather_at_coords(lat=location.lat, lon=location.lon).weather
-                if location.country == "US":
-                    temperature = weather.temperature('fahrenheit')
-                    wind = str(round(weather.wind(unit='miles_hour')['speed'],2)) + "mph"
-                    fltemp = str(temperature['feels_like']) + "\u00b0 F"
-                    rtemp = str(temperature['temp']) + "\u00b0 F"
-                else:
-                    temperature = weather.temperature('celsius')
-                    wind = str(round((weather.wind()['speed'] * 3.6),2)) + "kph"
-                    fltemp = str(temperature['feels_like']) + "\u00b0 C"
-                    rtemp = str(temperature['temp']) + "\u00b0 C"
-                forcast = "It is currently {0} and feels like {1}\n"\
-                      "The actual temperature is: {2} with a windspeed of {3}\n"\
-                      "The sun will set at {4}".format(weather.detailed_status,fltemp, rtemp, wind, weather.sunset_time(timeformat='date'))
-                await context.bot.send_message(update.message.chat_id, text=f"{forcast}")
-            else:
-                await context.bot.send_message(update.message.chat_id, text=f"Error finding location")
-    else:
-        await context.bot.send_message(update.message.chat_id, text=f"Please include a location with your command")
+async def rehash_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.from_user.username == "SL0RD":
+        logger.info("Rehash command triggered")
+        loadcommands(rehash=True)
+        await context.bot.send_message(update.message.chat_id, text=f"Rehash successfull")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -188,19 +180,20 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 def main() -> None:
     """Start the bot."""
+    global application
     application = Application.builder().token(TOKEN).build()
 
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("weather", weather_command))
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CommandHandler("cad", cad_command))
     application.add_handler(CommandHandler("usd", usd_command))
+    application.add_handler(CommandHandler("rehash", rehash_command))
 
+    loadcommands()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     application.add_error_handler(error_handler)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
+    
 
 if __name__ == '__main__':
     main()
---
+++ modules/module_weather.py
@@ -0,0 +1,86 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+import config
+from pyowm import OWM
+# from pyowm.utils import config as owmconfig
+# from pyowm.utils import timestamps
+
+config = config
+owm = OWM(config.OWM)
+mgr = owm.weather_manager()
+reg = owm.city_id_registry()
+#zip = mgr.weather_at_zip_code()
+
+COUNTRY_CODES = ['CA','US']
+STATE_CODES = ['AL','AK','AR','AZ','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IA','IN','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+
+def getLocation(location):
+    location = " ".join(location.split()[1:])
+    if len(location.split()[-1]) == 2:
+        country = location.split()[-1].upper()
+        location = " ".join(location.split()[:-1])
+        print(country)
+        print("Found country code")
+        if len(location.split()[-1]) == 2:
+            print ("Found state code")
+            state = location.split()[-1]
+            print(state)
+            location = " ".join(location.split()[:-1])
+            print(location)
+            locations = reg.locations_for(location, country=country, state=state, matching='exact')
+        else:
+            print(location)
+            locations = reg.locations_for(location, country=country, matching='exact')
+    else:
+        print("Error: no Country code found.")
+        country = ""
+
+    if locations:
+        return locations[0]
+    else:
+        return "Error, location not found"
+
+async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Get current weather for given location."""
+    message = update.message.text
+    if len(message.split()) > 1:
+        if message.split()[1].isdigit():
+            zipcode = message.split()[1]
+            weather = mgr.weather_at_zip_code(zipcode,'US').weather
+            print(weather)
+            temperature = weather.temperature('fahrenheit')
+            wind = str(round(weather.wind(unit='miles_hour')['speed'],2)) + "mph"
+            fltemp = str(temperature['feels_like']) + "\u00b0 F"
+            rtemp = str(temperature['temp']) + "\u00b0 F"
+
+            forcast = "It is currently {0} and feels like {1}\n"\
+                "The actual temperature is: {2} with a windspeed of {3}\n"\
+                "The sun will set at {4}".format(weather.detailed_status,fltemp, rtemp, wind, weather.sunset_time(timeformat='date'))
+            await context.bot.send_message(update.message.chat_id, text=f"{forcast}")
+        else:
+            location = getLocation(update.message.text)
+            if location != "Error, location not found":
+                print(location)
+                weather = mgr.weather_at_coords(lat=location.lat, lon=location.lon).weather
+                if location.country == "US":
+                    temperature = weather.temperature('fahrenheit')
+                    wind = str(round(weather.wind(unit='miles_hour')['speed'],2)) + "mph"
+                    fltemp = str(temperature['feels_like']) + "\u00b0 F"
+                    rtemp = str(temperature['temp']) + "\u00b0 F"
+                else:
+                    temperature = weather.temperature('celsius')
+                    wind = str(round((weather.wind()['speed'] * 3.6),2)) + "kph"
+                    fltemp = str(temperature['feels_like']) + "\u00b0 C"
+                    rtemp = str(temperature['temp']) + "\u00b0 C (" + str(round(temperature['temp']*1.8)+32) + "\u00b0 F)"
+                forcast = "It'ss currently {0} and feels like {1}\n"\
+                        "The actual temperature is: {2} with a windspeed of {3}\n"\
+                        "The sun will set at {4}".format(weather.detailed_status,fltemp, rtemp, wind, weather.sunset_time(timeformat='date'))
+                await context.bot.send_message(update.message.chat_id, text=f"{forcast}")
+            else:
+                await context.bot.send_message(update.message.chat_id, text=f"Error finding location")
+    else:
+        await context.bot.send_message(update.message.chat_id, text=f"Please include a location with your command")
+
+
