34596061441b47481d7b5e52fb3bae5f1de1638c
SL0RD <sl0rd@users.noreply.github.com>
2026-08-12 23:03:56 -0300

    Add Last.fm module with now playing and scrobble stats

--- example.conf.py
+++ example.conf.py
@@ -1,5 +1,6 @@
 TOKEN = ""  # Telegram Bot token from @BotFather
 OWM = ""  # OpenWeatherMap API key
 er_api_key = ""  # ExchangeRate-API key
+LASTFM_API_KEY = ""  # Last.fm API key from https://www.last.fm/api/account/create
 ADMIN_IDS = []  # List of numeric Telegram User IDs with admin privileges (e.g. [123456789])
 
